7a34d3786296cd73804dcc5f04868d8996112645
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-04-20 06:00:08 +0300

    created function create_copy

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ def create_copy(original_file_path: str, copy_file_path: str):
     """created copy of original_file_path in copy_file_path """
     with open(original_file_path, "rb") as file:
         original_data = file.read()
-
-
-
     with open(copy_file_path, "wb") as file:
         file.write(original_data)
+
+
+
+
+
 
 
 
