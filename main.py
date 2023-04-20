99853c92568df6b2b6cd0eed116ee426eb8f6cb6
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-04-20 05:59:04 +0300

    created function create_copy

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+# https://academy.masterschool.com/ns/books/published/swe/WritingFiles/7-HandlingBinaryFiles.html
 def main():
-    pass
+    create_copy("baseball.jpg", "baseball-copy.jpg")
 
 
+def create_copy(original_file_path: str, copy_file_path: str):
+    """created copy of original_file_path in copy_file_path """
+    with open(original_file_path, "rb") as file:
+        original_data = file.read()
+
+
+
+    with open(copy_file_path, "wb") as file:
+        file.write(original_data)
 
 
 
