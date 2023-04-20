2f683d2f01a35255e3ed81d217caf4430ea32006
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-04-20 06:04:17 +0300

    created function create_double_song

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # https://academy.masterschool.com/ns/books/published/swe/WritingFiles/7-HandlingBinaryFiles.html
 def main():
-    create_copy("baseball.jpg", "baseball-copy.jpg")
+    # create_copy("baseball.jpg", "baseball-copy.jpg")
+    create_double_song("sample.mp3", "sample-twice.mp3")
 
 
 def create_copy(original_file_path: str, copy_file_path: str):
@@ -11,7 +12,14 @@ def create_copy(original_file_path: str, copy_file_path: str):
         file.write(original_data)
 
 
-
+def create_double_song(original_file_path: str, copy_file_path: str):
+    """creating 2 repeated mp3 in one
+    from original_file_path to copy_file_path
+    """
+    with open(original_file_path, "rb") as file:
+        original_data = file.read()
+    with open(copy_file_path, "wb") as file:
+        file.write(original_data + original_data)
 
 
 
