1df794281ac19c02b3e00a7cf74478af4df2609e
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-04-20 06:21:46 +0300

    created function create_first_n_char

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # https://academy.masterschool.com/ns/books/published/swe/WritingFiles/7-HandlingBinaryFiles.html
 def main():
-    # create_copy("baseball.jpg", "baseball-copy.jpg")
+    create_copy("baseball.jpg", "baseball-copy.jpg")
     create_double_song("sample.mp3", "sample-twice.mp3")
+    create_first_n_char("baseball.jpg", "baseball-part.jpg", 30000)
 
 
 def create_copy(original_file_path: str, copy_file_path: str):
@@ -21,6 +22,14 @@ def create_double_song(original_file_path: str, copy_file_path: str):
     with open(copy_file_path, "wb") as file:
         file.write(original_data + original_data)
 
+
+def create_first_n_char(original_file_path: str, copy_file_path: str, first_n_chars: int):
+    """Create a copy of original_file_path under the name copy_file_path
+     that contains only the first_n_chars “characters”"""
+    with open(original_file_path, "rb") as file:
+        original_data = file.read()
+    with open(copy_file_path, "wb") as file:
+        file.write(original_data[:first_n_chars])
 
 
 
